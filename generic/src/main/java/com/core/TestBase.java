0f97808cb2a56f4b28ffe7c7121adf6d2c803589
shadman <shadman@shadmans-MBP.lan>
2021-06-13 11:42:54 -0400

    added report

--- generic/src/main/java/com/core/TestBase.java
+++ generic/src/main/java/com/core/TestBase.java
@@ -1,37 +1,135 @@
 package com.core;
 
+import com.core.report.ExtentManager;
+import com.core.report.ExtentTestManager;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
-import org.openqa.selenium.By;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
 
 public class TestBase {
 
-    public static  AppiumDriver driver;
+    private static final Logger LOGGER = Logger.getLogger(TestBase.class);
+    public static AppiumDriver driver;
+    public static ExtentReports extent;
 
-    @Parameters({"platform","deviceName","platformVersion"})
+    @Parameters({"platform", "deviceName", "platformVersion"})
     @BeforeMethod
-    public static void getAppiumDriver(String platform,String deviceName, String platformVersion) throws MalformedURLException {
+    public static void getAppiumDriver(String platform, String deviceName, String platformVersion) throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
-        if(platform.equalsIgnoreCase("android")) {
+        if (platform.equalsIgnoreCase("android")) {
             cap.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
             cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
             cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, platformVersion);
             cap.setCapability(MobileCapabilityType.APP_PACKAGE, "com.tdbank");
             cap.setCapability(MobileCapabilityType.APP_ACTIVITY, "com.td.dcts.android.us.app.SplashScreenActivity");
-            driver = new AndroidDriver(new URL("http://localhost:4724/wd/hub"), cap);
-        }else {
+            driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
+        } else {
             //code for ios
         }
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+    }
+
+
+    //screenshot
+    public static void captureScreenshot(WebDriver driver, String screenshotName) {
+        DateFormat dateFormat = new SimpleDateFormat("HH_mm_ss");
+        Date date = new Date();
+        // --> dateFormat.format(date);
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "/screenshots/" + screenshotName + " " + dateFormat.format(date) + ".jpg"));
+            System.out.println("Screenshot captured");
+        } catch (Exception e) {
+            System.out.println("Exception while taking screenshot " + e.getMessage());
+        }
+    }
+
+    //reporting starts
+    @BeforeSuite
+    public void extentSetup(ITestContext context) {
+        ExtentManager.setOutputDirectory(context);
+        extent = ExtentManager.getInstance();
+    }
+
+    @BeforeMethod
+    public void startExtent(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        ExtentTestManager.startTest(method.getName());
+        ExtentTestManager.getTest().assignCategory(className);
+    }
+
+    protected String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+
+    @AfterMethod
+    public void afterEachTestMethod(ITestResult result) {
+        ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getEndMillis()));
+        for (String group : result.getMethod().getGroups()) {
+            ExtentTestManager.getTest().assignCategory(group);
+        }
+
+        if (result.getStatus() == 1) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "Test Passed");
+        } else if (result.getStatus() == 2) {
+            ExtentTestManager.getTest().log(LogStatus.FAIL, getStackTrace(result.getThrowable()));
+        } else if (result.getStatus() == 3) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+        }
+
+        ExtentTestManager.endTest();
+        extent.flush();
+        if (result.getStatus() == ITestResult.FAILURE) {
+            captureScreenshot(driver, result.getName());
+        }
+    }
+
+    public Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+    //reporting finish
+
+    @AfterSuite
+    public void generateReport() {
+        extent.close();
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.quit();
+        LOGGER.info("driver closed");
     }
 }
--- td/src/test/java/com/td/tests/HomePageValidations.java
+++ td/src/test/java/com/td/tests/HomePageValidations.java
@@ -1,12 +1,17 @@
 package com.td.tests;
 
 import com.core.TestBase;
+import com.core.report.ExtentTestManager;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class HomePageValidations  extends TestBase {
+public class HomePageValidations extends TestBase {
+    private static final Logger LOGGER = Logger.getLogger(HomePageValidations.class);
+
 
     @Test
-    public void validateAppIsLaunching(){
+    public void validateAppIsLaunching() {
+        ExtentTestManager.log("app launched", LOGGER);
         System.out.println(driver.getPageSource());
     }
 }
